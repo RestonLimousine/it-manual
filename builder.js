1875214e456224f6768b8c098e351e5855848479
RestonLimousine <49453345+RestonLimousine@users.noreply.github.com>
2019-05-08 11:59:35 -0400

    Update builder.js

--- builder.js
+++ builder.js
@@ -301,7 +301,9 @@ function fetchAndBuildManual (url) {
       req = new XMLHttpRequest();
   req.onload = function () {
     buildManual(decodeURI(fileName), this.responseText);
-    // location.href = location.href;
+    var hash = location.hash;
+    location.hash = "";
+    location.hash = hash;
   };
   req.open("GET", url);
   req.send();
